757d9038cb6c7559e42b08916bdef0f65fc02a50
jameslove <hello@jameslove.co.uk>
2017-11-20 14:50:28 +0000

    refactor- tests - css - e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,11 +1,28 @@
 import { FeTestModulrFinancePage } from './app.po';
 
+
 describe('fe-test-modulr-finance App', () => {
-  let page: FeTestModulrFinancePage;
+    let page:FeTestModulrFinancePage;
 
-  beforeEach(() => {
-    page = new FeTestModulrFinancePage();
-  });
+    beforeEach(() => {
+        page = new FeTestModulrFinancePage();
+    });
 
-  
+
+    it('should list 3  navigation links', ()=> {
+        page.navigateTo();
+        let expectedResult = page.getNavBar().count();
+
+        expect(expectedResult).toBe(3);
+
+    });
+
+    it('should display a 5 table headers', ()=> {
+        page.navigateTo();
+        let expectedResult = page.getSearchResultsHeaders().count();
+
+        expect(expectedResult).toBe(5);
+
+    });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,22 @@
 import { browser, element, by } from 'protractor';
 
 export class FeTestModulrFinancePage {
-  navigateTo() {
-    return browser.get('/');
-  }
+    navigateTo() {
+        return browser.get('/');
+    }
 
-  getParagraphText() {
-    return element(by.css('h1 img'));
-  }
+    getParagraphText() {
+        return element(by.css('#test')).getText();
+    }
+
+    getNavBar() {
+
+        return element.all(by.css('nav ul li'));
+    }
+
+    getSearchResultsHeaders() {
+
+        return element.all(by.css('table#customerSearchResults tr th'));
+    }
+
 }
